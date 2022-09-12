3c7ec467ac8d63f514d42d6023cac9efbfe87e6e
Daniellira001 <106349786+Daniellira001@users.noreply.github.com>
2022-09-12 17:55:29 -0300

    Add files via upload

---
+++ modulo1.py
@@ -0,0 +1,10 @@
+
+idade = int(input("Digite sua idade: \n"))
+
+if idade >= 18:
+    print(f"você tem idade necessária! \n Você tem: {idade}")
+else:
+    print(f"idade insuficiente! \n você tem: {idade}")
+
+def idade():
+    return(idade)
